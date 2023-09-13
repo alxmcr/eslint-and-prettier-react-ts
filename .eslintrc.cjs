cd2c0fbbb2fd4e1b2b742f348e946b93e8bcf65a
Alejandro M. Coca <alxmcr@outlook.com>
2023-09-13 19:43:31 -0400

    ok

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,60 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "sonarjs", "filenames"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    indent: [
+      "error",
+      2,
+      {
+        VariableDeclarator: 2,
+      },
+    ],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "no-shadow": "error",
+    "no-nested-ternary": "error",
+    "no-unused-expressions": "error",
+    "no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "all",
+        ignoreRestSiblings: false,
+        argsIgnorePattern: "[_]",
+      },
+    ],
+    // Extra rules
+    "filenames/match-exported": "error",
+    "sonarjs/cognitive-complexity": "error",
+    "sonarjs/prefer-immediate-return": "error",
+    "sonarjs/no-duplicate-string": "error",
+    "sonarjs/no-gratuitous-expressions": "error",
+    "sonarjs/no-identical-expressions": "error",
+  },
+};
---
+++ src/helpers.ts
@@ -0,0 +1,5 @@
+const hola = (name = "") => {
+    return `Hola ${name}`
+}
+console.log("🚀 ~ file: helpers.ts:4 ~ hola ~ hola:", hola(1))
+
