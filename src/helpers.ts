600662f50227a18089097e59ccc0487c6a25bbd1
Alejandro M. Coca <alxmcr@outlook.com>
2023-09-13 21:48:27 -0400

    improvements

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,10 +1,13 @@
 const hola = (name = '') => {
-  const nu = 5;
   return `Hola ${name}`;
 };
 
 const cassete = (artist = '') => {
   return `Playing... ${artist}`;
 };
+console.log(
+  '🚀 ~ file: helpers.ts:8 ~ cassete ~ cassete:',
+  cassete('bruno mars'),
+);
 
 console.log('🚀 ~ file: helpers.ts:4 ~ hola ~ hola:', hola('juan'));
